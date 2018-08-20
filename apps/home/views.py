e084f609d28f2052269e07169486e35738e5c9a8
Shawnpcw <s.cannon.williams@gmail.com>
2018-08-20 12:58:22 -0700

    working product

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
 
-# Create your views here.
+def index(request):
+        
+    return HttpResponse("hello")
