d80ce0eb523967c4acf3c3046ed3a2a752e52eea
Caitlin Stewart <cdestewart3@gmail.com>
2018-08-21 13:40:33 -0700

    Added player 1 and player 2 html files

--- apps/game/urls.py
+++ apps/game/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views    
 app_name = 'game'     
 urlpatterns = [
-    url(r'^game$', views.index, name='index')
+    url(r'^computer$', views.index, name='index'),
+    url(r'^player1$', views.player1, name='player1'),
+    url(r'^player2$', views.player2, name='player2')
 ]
--- apps/game/views.py
+++ apps/game/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'game/index.html')
+
+def player1(request):
+    return render(request,'game/player1.html')
+
+def player2(request):
+    return render(request,'game/player2.html')
