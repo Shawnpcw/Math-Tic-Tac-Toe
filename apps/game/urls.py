995e2139bed21cbe58622d7869a13c3c1ec8410f
Caitlin Stewart <cdestewart3@gmail.com>
2018-08-23 15:13:33 -0700

    updates to the database and the game page

--- apps/game/urls.py
+++ apps/game/urls.py
@@ -5,5 +5,6 @@ app_name = 'game'
 urlpatterns = [
     url(r'^computer$', views.index, name='index'),
     url(r'^player1/(?P<num>\d+)$', views.multiplayer, name='multiplayer'),
-    url(r'^player2/(?P<num>\d+)$', views.player2, name='player2')
+    url(r'^player2/(?P<num>\d+)$', views.player2, name='player2'),
+    url(r'^update$', views.update, name='update')
 ]
