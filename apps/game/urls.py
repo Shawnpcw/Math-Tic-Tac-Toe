55817da42f22021cafc40a95f6049d8f2677c60c
Shawnpcw <s.cannon.williams@gmail.com>
2018-08-23 16:36:43 -0700

    something

--- apps/game/urls.py
+++ apps/game/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^computer$', views.index, name='index'),
     url(r'^player1/(?P<num>\d+)$', views.multiplayer, name='multiplayer'),
     url(r'^player2/(?P<num>\d+)$', views.player2, name='player2'),
-    url(r'^update$', views.update, name='update')
+    url(r'^update$', views.update, name='update'),
+    url(r'^update1$', views.update1, name='update1')
 ]
