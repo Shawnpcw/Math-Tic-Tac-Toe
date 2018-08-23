5bdce4eb7e0b9db62e63fe3d7bceeeb98d4052d3
Shawnpcw <s.cannon.williams@gmail.com>
2018-08-22 17:47:47 -0700

    working matches

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -4,15 +4,11 @@ app_name = 'home'
 urlpatterns = [
 
     url(r'^$', views.index, name='index')  ,
-<<<<<<< HEAD
-    url(r'^stats$', views.stats, name='stats')  ,
-    url(r'^start_game$', views.start_game, name='start_game')  ,
-=======
-    url(r'^stats$', views.stats, name='stats'),
-    url(r'^matchRoom$', views.matchRoom, name='matchRoom'),
-    url(r'^createMatch$', views.createMatch, name='createMatch'),
-    url(r'^add$', views.add, name='add')
->>>>>>> origin/Cbranch
+    url(r'^/stats$', views.stats, name='stats'),
+    url(r'^/matchRoom$', views.matchRoom, name='matchRoom'),
+    url(r'^/createMatch$', views.createMatch, name='createMatch'),
+    url(r'^/add$', views.add, name='add'),
+    url(r'^/logout$', views.logout, name='logout'),
 
 
 ] 
